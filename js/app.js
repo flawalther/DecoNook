2977aa0630b9efdf9e03e5a711968ed1d5445fe4
Laura GL <laugranadolopez@gmail.com>
2023-09-01 20:08:16 +0200

    ⬆️Cambiando estilos img y probando modal (fallido)

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ import { productos } from "../data/productos.js";
 let contenedorDestacados = document.querySelector("#content-destacados");
 
 const cargarProducto = () => {
-  productos.map((producto, index) => {
+  productos.map((producto) => {
     let col = document.createElement("div");
     col.classList = "col";
 
     let tarjeta = `<div class=card h100>
-        <img src=${producto.imagen} class="card-img-top" alt=${producto.nombre}>
+        <img src=${producto.imagen} class="card-img-top" alt=${producto.nombre}>        
         <div class="card-body">
         <h6 class="card-title">${producto.nombre}><h6>
         <p class="card-text">${producto.descripcion}</p>
@@ -17,6 +17,15 @@ const cargarProducto = () => {
     col.innerHTML = tarjeta;
     contenedorDestacados.append(col);
   });
+};
+
+const mostralModal = () => {
+  event.preventDefault();
+  document.querySelector(productos).value = productos[index].nombre;
+  document.querySelector(productos).value = productos[index].precio;
+  document.querySelector(productos).value = productos[index].imagen;
+  document.querySelector(productos).value = productos[index].descripcion;
+  myModal.show();
 };
 
 cargarProducto();
