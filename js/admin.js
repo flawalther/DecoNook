54a0506c960656b9ad2f22374969a2dc6828deea
alex@delcampoalacocina.com <alex@delcampoalacocina.com>
2023-09-05 19:53:59 +0200

    añado favicon y pagina inicio sesion y admin

---
+++ js/admin.js
@@ -0,0 +1,26 @@
+let auth = JSON.parse(localStorage.getItem("auth")) || null;
+let contenido = document.querySelector("#main");
+let boton = document.querySelector("#logOut");
+
+// let peli1 = new Pelicula();
+
+if (!auth) {
+  boton.classList = "d-none";
+  contenido.innerHTML = "";
+  let mensaje = document.createElement("div");
+  mensaje.classList = "alert alert-danger";
+  mensaje.role = "alert";
+  mensaje.innerText =
+    "Lo siento, no tiene permisos para acceder a esta sección";
+  contenido.append(mensaje);
+}
+
+const cerrarSesion = () => {
+  //borrar datos del usuario en localstorage
+  //redireccionar al home
+
+  localStorage.removeItem("auth");
+  location.replace("/");
+};
+
+boton.addEventListener("click", cerrarSesion);
