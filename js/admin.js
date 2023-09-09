d096752a6032c7fbb1b08122329c06bdd09f883c
Flavia Walther <flamwalther@gmail.com>
2023-09-09 11:18:50 -0300

    se trabaja en el CRUD

--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,14 @@
 import { productos, Producto } from "../data/productos.js";
 
-
-
 localStorage.setItem("productos", JSON.stringify(productos));
-
 
 let tabla = document.querySelector("#tablaAdm");
 
 const myModal = new bootstrap.Modal(document.getElementById('productoModal'));
+const myModal2 = new bootstrap.Modal(document.getElementById('productoModal2'));
 
 const mostrarModal = ()=>{
-        document.querySelector("form").reset();
+        document.getElementById("form1").reset();
         myModal.show();
 }
 
@@ -26,7 +24,7 @@ const cargarTabla = () => {
          let celdas = `<td>${producto.codigo}</td><td>${producto.nombre}</td><td>${producto.precio}</td>
          <td>${producto.categoria}</td><td>${producto.stock}</td>
          <td><button class="btn btn-danger btn-sm mx-1" onclick="borrarProducto(${index})">X</button></td>
-         <td><button class="btn btn-warning btn-sm px-1" onclick="modificarProducto(${index})">M</button></td>`;
+         <td><button class="btn btn-warning btn-sm px-1" onclick="mostrarProducto(${index})">M</button></td>`;
          fila.innerHTML = celdas;
          tabla.append(fila);
     });
@@ -50,18 +48,45 @@ window.guardarProducto = (event) => {
     localStorage.setItem("productos",JSON.stringify(productos));
 
     cargarTabla();
+
+    myModal.hide();
+
 };
 
-window.modificarProducto = (index)=>{
-    document.querySelector("#inputcodigo").value = productos[index].codigo;
-    document.querySelector("#inputnombre").value = productos[index].nombre;
-    document.querySelector("#inputprecio").value = productos[index].precio;
-    document.querySelector("#inputcategoria").value = productos[index].categoria;
-    document.querySelector("#inputimagen").value = productos[index].imagen;
-    document.querySelector("#inputdescripcion").value = productos[index].descripcion;
-    document.querySelector("#inputstock").value = productos[index].stock;
-    myModal.show();
+window.modificarProducto = (event)=>{
+    event.preventDefault();
     
+    productos[posicion].codigo = document.getElementById("#inputcodigo2").value;
+    productos[posicion].nombre = document.getElementById("#inputnombre2").value;
+    productos[posicion].precio = document.getElementById("#inputprecio2").value;
+    productos[posicion].categoria = document.getElementById("#inputcategoria2").value;
+    productos[posicion].imagen = document.getElementById("#inputimagen2").value;
+    productos[posicion].descripcion = document.getElementById("#inputdescripcion2").value;
+    productos[posicion].stock = document.getElementById("#inputstock2").value;
+// console.log(posicion)
+
+    localStorage.setItem("productos",JSON.stringify(productos));
+
+    myModal2.hide();
+
+    cargarTabla();
+
+};
+
+let posicion = null;
+
+window.mostrarProducto = (index) =>{
+    posicion = index;
+
+    document.querySelector("#inputcodigo2").value = productos[index].codigo;
+    document.querySelector("#inputnombre2").value = productos[index].nombre;
+    document.querySelector("#inputprecio2").value = productos[index].precio;
+    document.querySelector("#inputcategoria2").value = productos[index].categoria;
+    document.querySelector("#inputimagen2").value = productos[index].imagen;
+    document.querySelector("#inputdescripcion2").value = productos[index].descripcion;
+    document.querySelector("#inputstock2").value = productos[index].stock;
+    
+    myModal2.show();
 };
 
 window.borrarProducto =(index)=>{
